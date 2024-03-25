b05d4213bbb826d24dc5678bb58b4cbc90f2e7b9
MouriNaruto <Kenji.Mouri@outlook.com>
2024-03-25 21:22:21 +0800

    Implement Mile.Detours.RefreshPackageVersion.

--- Mile.Detours.RefreshPackageVersion/Program.cs
+++ Mile.Detours.RefreshPackageVersion/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Mile.Detours.RefreshPackageVersion
+﻿using Mile.Project.Helpers;
+using System.Xml;
+
+namespace Mile.Detours.RefreshPackageVersion
 {
     internal class Program
     {
+        private static string RepositoryRoot = GitRepository.GetRootPath();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string NuspecFilePath = string.Format(
+                @"{0}\Mile.Detours.NuGet\Mile.Detours.nuspec",
+                RepositoryRoot);
+
+            string VersionTag = string.Empty;
+            //string VersionTag = "-preview";
+
+            string VersionString = string.Format(
+                "1.0.{0}.0{1}",
+                DateTime.Today.Subtract(DateTime.Parse("2020-01-04")).TotalDays,
+                VersionTag);
+
+            XmlDocument Document = new XmlDocument();
+            Document.PreserveWhitespace = true;
+            Document.Load(NuspecFilePath);
+
+            XmlNode? PackageNode = Document["package"];
+            if (PackageNode != null)
+            {
+                XmlNode? MetadataNode = PackageNode["metadata"];
+                if (MetadataNode != null)
+                {
+                    XmlNode? VersionNode = MetadataNode["version"];
+                    if (VersionNode != null)
+                    {
+                        VersionNode.InnerText = VersionString;
+                    }
+                }
+            }
+
+            Document.Save(NuspecFilePath);
+
+            Console.WriteLine(
+                "Mile.Detours.RefreshPackageVersion task has been completed.");
             Console.ReadKey();
         }
     }
